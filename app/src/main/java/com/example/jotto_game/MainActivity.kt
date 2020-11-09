064f089058ddbf854e623f9011959008607e08f2
AygulMalikova <amalikova19@gmail.com>
2020-11-09 16:27:59 +0300

    Feature: adding layout for the main activity Feature: designing and adding animation the main page

--- app/src/main/java/com/example/jotto_game/MainActivity.kt
+++ app/src/main/java/com/example/jotto_game/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.jotto_game
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButtonAnimation()
+
+    }
+
+    //https://stackoverflow.com/questions/27301586/repeat-pulse-animation
+    private fun startButtonAnimation() {
+        val startBtn: Button = findViewById<Button>(R.id.start_button)
+
+        val scaleDown: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+                startBtn,
+                PropertyValuesHolder.ofFloat("scaleX", 1.2f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.2f))
+        scaleDown.duration = 700
+
+        scaleDown.repeatCount = ObjectAnimator.INFINITE
+        scaleDown.repeatMode = ObjectAnimator.REVERSE
+
+        scaleDown.start()
     }
 }
