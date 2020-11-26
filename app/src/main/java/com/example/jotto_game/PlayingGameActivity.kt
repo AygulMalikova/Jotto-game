6e89e1ecb447e6c0186215e4ecee7ef482829287
Ваше Имя <anufriyevt@yandex.ru>
2020-11-27 01:40:33 +0300

    added some checks and finish button

--- app/src/main/java/com/example/jotto_game/PlayingGameActivity.kt
+++ app/src/main/java/com/example/jotto_game/PlayingGameActivity.kt
@@ -23,13 +23,28 @@ class PlayingGameActivity : AppCompatActivity() {
 
         check_word_button.setOnClickListener {
             val similarCount = checkWord(str)
-            val item = ExampleItem(word_tocheck.text.toString(), similarCount.toString())
-            wordList += item
-            // TODO: adapter does not updates, hz why, will fix
-            recycler_view.adapter?.notifyDataSetChanged()
+            if (similarCount == str.length) {
+                finishGame()
+            }
+            if (similarCount != -1) {
+                val item = ExampleItem(word_tocheck.text.toString(), similarCount.toString())
+                wordList += item
+                // TODO: adapter does not updates, hz why, will fix
+                recycler_view.adapter?.notifyItemChanged(wordList.size - 1)
+            }
+        }
+
+        give_up_button.setOnClickListener {
+            finishGame()
         }
     }
 
+    private fun finishGame() {
+        Toast.makeText(
+            this, "Game is finished",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
 
     private fun generateWord(): String {
         //  TODO: generate word
@@ -40,36 +55,62 @@ class PlayingGameActivity : AppCompatActivity() {
     private fun checkWord(generated: String): Int {
         // TODO: check if the word exists and does not contain repeated letters
         var word = word_tocheck.text.toString()
-        if (word.isEmpty()) {
-            Toast.makeText(
-                this, "Please, enter the word first",
-                Toast.LENGTH_SHORT
-            ).show()
-        } else if (word.length != generated.length) {
-            Toast.makeText(
-                this, "Please, enter the word with length:" +
-                        generated.length.toString(), Toast.LENGTH_SHORT
-            ).show()
-        }
         word = word.toLowerCase(Locale.ROOT)
+        when {
+            word.isEmpty() -> {
+                Toast.makeText(
+                    this, "Please, enter the word first",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return -1
+            }
+            word.length != generated.length -> {
+                Toast.makeText(
+                    this, "Please, enter the word with length:" +
+                            generated.length.toString(), Toast.LENGTH_SHORT
+                ).show()
+                return -1
+            }
+            hasSimilarLetters(word) -> {
+                Toast.makeText(
+                    this, "Word contains similar letters", Toast.LENGTH_SHORT
+                ).show()
+                return -1
+            }
+            else -> {
+                val hashMap: HashMap<Char, Int> = HashMap<Char, Int>()
+                for (i in word.indices) {
+                    if (!hashMap.containsKey(word[i]))
+                        hashMap[word[i]] = 1
+                    else {
+                        val value = hashMap[word[i]]
+                        hashMap.remove(word[i])
+                        if (value != null) {
+                            hashMap[word[i]] = value + 1
+                        }
+                    }
+                }
+                var counter = 0
+                for (i in generated.indices) {
+                    if (hashMap.containsKey(generated[i]))
+                        counter += 1
+                }
+                return counter
+            }
+        }
 
+    }
+
+    private fun hasSimilarLetters(word: String): Boolean {
         val hashMap: HashMap<Char, Int> = HashMap<Char, Int>()
         for (i in word.indices) {
             if (!hashMap.containsKey(word[i]))
                 hashMap[word[i]] = 1
             else {
-                val value = hashMap[word[i]]
-                hashMap.remove(word[i])
-                if (value != null) {
-                    hashMap[word[i]] = value + 1
-                }
+                return true
             }
         }
-        var counter = 0
-        for (i in generated.indices) {
-            if (hashMap.containsKey(generated[i]))
-                counter += 1
-        }
-        return counter
+        return false
+
     }
 }
