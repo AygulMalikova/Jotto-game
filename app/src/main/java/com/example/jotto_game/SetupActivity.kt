302130d72a1e391f62fa81b8c07aead0460027a7
AygulMalikova <amalikova19@gmail.com>
2020-12-08 22:39:55 +0300

    Feature: Improved view of the setup page

--- app/src/main/java/com/example/jotto_game/SetupActivity.kt
+++ app/src/main/java/com/example/jotto_game/SetupActivity.kt
@@ -3,12 +3,13 @@ package com.example.jotto_game
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
+import com.skydoves.balloon.ArrowOrientation
+import com.skydoves.balloon.BalloonAnimation
+import com.skydoves.balloon.createBalloon
+import com.skydoves.balloon.showAlignBottom
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner
 
 
@@ -18,6 +19,8 @@ class SetupActivity : AppCompatActivity() {
     var difficulties = arrayOf("Easy", "Medium", "Hard")
     var selectedDifficulty = ""
 
+    val helperText = "Different difficulty of the game depends on the number of attempts. " +
+            "The easy option has no limitations, medium has 15 attempts and the hard 7."
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +34,27 @@ class SetupActivity : AppCompatActivity() {
             startGame()
         }
 
+        val helperButton: ImageButton = findViewById<ImageButton>(R.id.helper)
+
+        helperButton.setOnClickListener {
+            val balloon = createBalloon(baseContext) {
+                setArrowSize(10)
+                setWidthRatio(0.8f)
+                setArrowPosition(0.85f)
+                setCornerRadius(7f)
+                setAlpha(0.9f)
+                setText(helperText)
+                setTextSize(18f)
+                setTextColorResource(R.color.black)
+                setArrowOrientation(ArrowOrientation.TOP)
+                setBackgroundColorResource(R.color.design_default_color_background)
+                setBalloonAnimation(BalloonAnimation.FADE)
+                setPadding(10)
+                setLifecycleOwner(lifecycleOwner)
+            }
+            helperButton.showAlignBottom(balloon)
+        }
+
     }
 
     /**
@@ -39,7 +63,7 @@ class SetupActivity : AppCompatActivity() {
     private fun initInput() {
         val numberOfWordsInput: EditText = findViewById<EditText>(R.id.number_of_words_input)//finding the start game button
 
-        numberOfWordsInput.setText("5"); // 5 is a default number of letters in the word
+        numberOfWordsInput.setText(getString(R.string.default_letter)); // 5 is a default number of letters in the word
 
         val til =
             findViewById<View>(R.id.textInputLayout) as TextInputLayout
@@ -48,6 +72,9 @@ class SetupActivity : AppCompatActivity() {
             til.error = message
             startBtn.isEnabled = false
         }
+
+        val minLetters = getString(R.string.min_letter).toInt()
+        val maxLetters = getString(R.string.max_letter).toInt()
 
         numberOfWordsInput.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             val currentValue = numberOfWordsInput.text.toString().toIntOrNull()
@@ -61,9 +88,9 @@ class SetupActivity : AppCompatActivity() {
             if (currentValue == null) {
                 showError(getString(R.string.not_int))
             } else {
-                if (currentValue < 3) {
+                if (currentValue < minLetters) {
                     showError(getString(R.string.too_few))
-                } else if (currentValue > 10 ) {
+                } else if (currentValue > maxLetters ) {
                     showError(getString(R.string.too_much))
                 }
             }
@@ -87,10 +114,16 @@ class SetupActivity : AppCompatActivity() {
 
         spinner.setAdapter(spinnerAdapter)
 
+
+
         spinner.setOnItemClickListener(AdapterView.OnItemClickListener { adapterView, view, position, l ->
             selectedDifficulty =
                 adapterView.getItemAtPosition(position).toString()
         })
+
+        // setSelection is not working for better spinner https://github.com/Lesilva/BetterSpinner/issues/92
+        spinner.getEditableText().append("Easy"); //setting the first choice as a default value
+
     }
 
     /**
