44e1047c8d4e8a3eb9eaf47811cefdb5c5509755
elukyanchikova <lukyanchikowae@gmail.com>
2020-12-15 07:12:35 +0300

    adde instrumented tests for FinishGameActivity

--- app/src/androidTest/java/com/example/jotto_game/FinishGameActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/jotto_game/FinishGameActivityInstrumentedTest.kt
@@ -1,24 +1,24 @@
 package com.example.jotto_game
 
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.provider.Settings.Global.getString
+import android.os.Build
+import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.ActivityResultMatchers.hasResultCode
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.example.jotto_game.finishgame.view.FinishGameActivity
-import org.junit.Assert.*
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
 
 
 /**
@@ -29,49 +29,64 @@ import java.lang.Thread.sleep
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class FinishGameActivityInstrumentedTest {
+    var context: Context? = null
+
     @get:Rule
     val activityRule = ActivityTestRule(FinishGameActivity::class.java)
 
-//    @Test
-//    fun resutTextView() {
-//        val activityUnderTest: FinishGameActivity = activityRule.activity
-//        activityUnderTest.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
-//
-//
-//        val extras = activityRule.activity.intent.extras
-//        val word: String
-//        val numberOfTries: Int
-//        val userWin: Boolean
-//        if (extras != null) {
-//            word = extras.getString(R.string.sourceWord.toString()).toString()
-//            numberOfTries = extras.getInt(R.string.numberOfAttempts.toInt().toString()).toInt()
-//            userWin = extras.getBoolean(R.string.gameResult.toString())
-//
-//        } else {
-//            word = "None"
-//            numberOfTries = -1
-//            userWin = false
-//        }
-//        if (userWin) {
-//            val res =
-//                activityRule.activity.resources.getString(R.string.winMessage, numberOfTries, word)
-//            onView(withId(R.id.finishTextView)).check(matches(withText(res)))
-//        } else {
-//
-//            val res =
-//                activityRule.activity.resources.getString(R.string.loseMessage, numberOfTries, word)
-//            val tr = onView(withId(R.id.finishTextView)).toString()
-//            onView(withId(R.id.finishTextView)).check(matches(withText(res)))
-//        }
-//    @Test
-//    fun pressHome_openSetupActivity() {
-//
-//        val activityUnderTest: FinishGameActivity = activityRule.activity
-//        activityUnderTest.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
-//        onView(withId(R.id.finishButton)).perform(click())
-//
-//            }
+    @Before
+    fun setup() {
+        val activityUnderTest: FinishGameActivity = activityRule.activity
+        activityUnderTest.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
+
+    }
+
+    /**
+     * test the depicting of resultPictureView
+     */
+    @Test
+    fun resultPictureView() {
+        onView(withId(R.id.finishImageView)).check(matches(allOf(isDisplayed())))
+    }
+
+    /**
+     * test the depicting of resultTextView
+     */
+    @Test
+    fun resultTextView() {
+        onView(withId(R.id.finishTextView)).check(matches(isDisplayed()))
+        val t = onView(withId(R.id.finishTextView)).toString()
+        assertTrue(t.isNotEmpty())
+
+    }
+
+    /**
+     * test the depicting of homeButton
+     */
+    @Test
+    fun homeButtonView() {
+        onView(withId(R.id.finishButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.finishButton)).check(matches(withText(R.string.finishHomeBtn)))
+    }
+
+    /**
+     * test finishing of FinishGameActivity by press the home button
+     */
+    @Test
+    fun pressHome_openSetupActivity() {
+
+        val activityUnderTest: FinishGameActivity = activityRule.activity
+        activityUnderTest.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
+        onView(withId(R.id.finishButton)).perform(click())
+
+        Thread.sleep(2000)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            assertTrue(activityRule.activity.isDestroyed)
+        }
+    }
 
 }
+
 
 
